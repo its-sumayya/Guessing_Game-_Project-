58dbcc432ad59ece279a83075df30d79d4c5b997
its-sumayya <sumayyamaroc@gmail.com>
2025-11-01 13:12:00 +0100

    Delete generate_readme.sh

--- generate_readme.sh
+++
@@ -1,8 +0,0 @@
-
-
-echo "# Guessing Game Project" > README.md
-echo "" >> README.md
-echo "**Date and Time:** $(date)" >> README.md
-echo "" >> README.md
-echo "**Number of lines in guessinggame.sh:** $(wc -l < guessinggame.sh)" >> README.md
-
